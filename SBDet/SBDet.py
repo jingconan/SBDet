1fa3aecbb1ce97dbc3df1fad472ae832d72bb605
Jing Conan Wang <wangjing@bu.edu>
2013-12-02 15:38:21 -0500

    Refactor: SBDet.py cal_SIG_low_mem

--- SBDet/SBDet.py
+++ SBDet/SBDet.py
@@ -20,8 +20,9 @@ from .Util import DataEndException
 from .Util import igraph
 from .Util import sp, la, stats
 
-def cal_SIG_low_mem(data_file, interval=10.0, dur=10.0, rg=(0.0, float('inf')),
-            directed=False, folder=None):
+
+def cal_SIG_low_mem(data_file, interval=10.0, dur=10.0,
+                    rg=(0.0, float('inf')), folder=None):
     """ Calculate the Social Interaction Graph (SIG)
 
     Parameters
@@ -32,44 +33,54 @@ def cal_SIG_low_mem(data_file, interval=10.0, dur=10.0, rg=(0.0, float('inf')),
         interval between two windows and the duration of each window
     rg : tuple (start_time, end_time)
         Only flows whose timestamps belong to [start_time, end_time) are used.
-    directed : bool
-        if true, the SIG is directed, otherwise it is undirected.
     folder : str
-        folder name for the output
-
-
-    Returns
-    --------------
-    sigs : list of Graphs with format specified by tp
-
+        path for the output folder
     """
-    # FIXME directed parameter is useless
     start_time, end_time = rg
-    if end_time == float('inf'):
-        N = sys.maxint
-    else:
-        N = int((end_time - start_time) // dur)
+    N = sys.maxint if end_time == float('inf') \
+        else int((end_time - start_time) // dur)
 
-    TGraph = NetworkXGraph
-    ips = TGraph(data_file).get_vertices()
-    # ips = [np_to_dotted(ip) for ip in ips]
+    ips = NetworkXGraph(data_file).get_vertices()
     dump(ips, folder + 'nodes.pk')
     try:
         for i in xrange(N):
-            print('i', i)
-            if N != sys.maxint: progress_bar(i * 1.0 / N * 100)
-            else: progress_bar(i * 1.0)
-            tg = TGraph(data_file)
+            sys.stdout.write("[%d]" % i)
+            sys.stdout.flush()
+
+            tg = NetworkXGraph(data_file)
             tg.add_vertices(ips)
-            records = tg.filter(prot=None,
-                                rg=[start_time + i * interval, start_time + i * interval + dur],
-                                rg_type='time')
+            seg = [start_time + i * interval, start_time + i * interval + dur]
+            records = tg.filter(prot=None, rg=seg, rg_type='time')
             edges = tg.get_edges(records)
             tg.add_edges(edges)
             dump({'edges': edges}, '%s%i.pk' % (folder, i))
-            # import ipdb;ipdb.set_trace()
     except DataEndException:
         print('reach end')
+
+    sys.stdout.write('\n')
+
+
+
+def pack_sigs(folder, out):
+    """  pack the sequence of sigs in a **folder** into a single file
+    """
+    N = sys.maxint
+    try:
+        nodes = load('%snodes.pk' % (folder))
+        nm = dict(zip(nodes, range(len(nodes))))
+        sigs = []
+        for i in xrange(N):
+            if i % 100 == 0:
+                sys.stdout.write("\r%d" % i)
+                sys.stdout.flush()
+            dat = load('%s%i.pk' % (folder, i))
+            ce = lambda edge: (nm[edge[0]], nm[edge[1]])
+            sigs.append([(ce(e), c) for e, c in dat['edges'].iteritems()])
+    except IOError:
+        pass
+    dump({'nodes': nodes, 'sig_edges': sigs}, out)
+    sys.stdout.write('\n')
+
 
 
 def cal_SIG(data_file, interval=10.0, dur=10.0, rg=(0.0, float('inf')),
