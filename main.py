288905ca4e26c89b8c6b159fa99d4f890714bb9e
wangjing <wangjing AT bu.edu>
2013-12-15 18:42:37 -0500

    Add example  main.py script

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ from SBDet import mix, load, dump
 if __name__ == "__main__":
     normal_sigs, normal_nodes = parseToCoo('../pcap2sigs-loc6-20070501-2055.sigs',
             undirected=True)
-    # botnet_sigs, botnet_nodes = parseToCoo('../ddostrace.20070804_134936-10.sigs', undirected=True)
-    # mix_sigs, mix_nodes = mix((normal_sigs, normal_nodes), (botnet_sigs, botnet_nodes), 0)
-    # dump(dict(sigs=mix_sigs, nodes=mix_nodes), './mix_sigs.pk')
+    botnet_sigs, botnet_nodes = parseToCoo('../ddostrace.20070804_134936-10.sigs', undirected=True)
+    mix_sigs, mix_nodes = mix((normal_sigs, normal_nodes),
+                              (botnet_sigs, botnet_nodes), 30)
+    dump(dict(sigs=mix_sigs, nodes=mix_nodes), './mix_sigs.pk')
     # import ipdb;ipdb.set_trace()
     data = load('./mix_sigs.pk')
     # import ipdb;ipdb.set_trace()
     model, para = select_model(normal_sigs[:3])
-    divs = monitor_deg_dis(data['sigs'], model, para)
+    print('model: ', model, ' para: ', para)
+    divs = monitor_deg_dis(data['sigs'][0:100], model, [para, 1e-10])
+    print('divs', divs)
+    # divs = monitor_deg_dis(data['sigs'][0:100], 'BA', [0.1, 1e-10])
+    import matplotlib.pyplot as plt
+    plt.plot(divs)
     import ipdb;ipdb.set_trace()
 
     pass
